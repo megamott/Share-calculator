552ee4f548c343f1e763f285aa139c19440164ef
Matvey Konoplyov <megamott27@gmail.com>
2020-08-30 14:03:13 +0300

    Initial commit. Added necessary maven dependencies.

---
+++ src/test/java/org/money/stockcalculator/StockCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.money.stockcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
